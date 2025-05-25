71e0e42078864db9a40b2e07200c85c9c425e50c
Ahammad Sabik <sabik.mh@gmail.com>
2025-05-25 14:22:01 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -27,7 +27,7 @@ function Projects() {
               isBlog={false}
               title="Textutils"
               description="TextUtils is a ReactJs website built primarily to do various operations on regular typed text. You can safely use WordPad or NotePad for text drafting, and saving, but TextUtils offers much more than simple text drafting and formatting. TextUtils can convert your text to any case in just one simple click of a button. "
-              ghLink=""
+              ghLink="https://github.com/sabik-ahmd/textutils.git"
             />
           </Col>
 
